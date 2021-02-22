3ff894a057beaa54011f69da50eb89ce1f9c7d7f
LESTHER-WB2020 XM <lesther.manuel@galileo.edu>
2021-02-22 14:20:34 -0600

    implementación de producto,resta y division

--- index.js
+++ index.js
@@ -2,12 +2,37 @@ const mousetrap = require('mousetrap');
 
 mousetrap.bind('1',clickUno);
 mousetrap.bind('2',clickDos);
+mousetrap.bind('3',clickTres);
+mousetrap.bind('4',clickCuatro);
+mousetrap.bind('5',clickCinco);
+mousetrap.bind('6',clickSeis);
+mousetrap.bind('7',clickSiete);
+mousetrap.bind('8',clickOcho);
+mousetrap.bind('9',clickNueve);
+mousetrap.bind('0',clickCero);
 mousetrap.bind('+',clickSuma);
-
+mousetrap.bind('-',clickResta);
+mousetrap.bind('*',clickProducto);
+mousetrap.bind('/',clickDivision);
 
 document.getElementById('botonUno').addEventListener('click',clickUno);
 document.getElementById('botonDos').addEventListener('click',clickDos);
+document.getElementById('botonTres').addEventListener('click',clickTres);
+document.getElementById('botonCuatro').addEventListener('click',clickCuatro);
+document.getElementById('botonCinco').addEventListener('click',clickCinco);
+document.getElementById('botonSeis').addEventListener('click',clickSeis);
+document.getElementById('botonSiete').addEventListener('click',clickSiete);
+document.getElementById('botonOcho').addEventListener('click',clickOcho);
+document.getElementById('botonNueve').addEventListener('click',clickNueve);
+document.getElementById('botonCero').addEventListener('click',clickCero);
+document.getElementById('botonBack').addEventListener('click',clickBack);
+document.getElementById('botonClean').addEventListener('click',clickClean);
+
+
 document.getElementById('suma').addEventListener('click',clickSuma);
+document.getElementById('resta').addEventListener('click',clickResta);
+document.getElementById('producto').addEventListener('click',clickProducto);
+document.getElementById('division').addEventListener('click',clickDivision);
 //IT´S THE SAME THING LIKE: document.getElementById('botonUno').onclick=clickUno;
 
 var numActual = document.getElementById('numeroActual');
@@ -26,6 +51,48 @@ function clickDos(){
         numActual.innerHTML = actual;
 }
 
+function clickTres(){
+    actual +=3;
+        numActual.innerHTML = actual;
+}
+
+function clickCuatro(){
+    actual +=4;
+        numActual.innerHTML = actual;
+}
+
+function clickCinco(){
+    actual +=5;
+        numActual.innerHTML = actual;
+}
+
+function clickSeis(){
+    actual +=6;
+        numActual.innerHTML = actual;
+}
+
+function clickSiete(){
+    actual +=7;
+        numActual.innerHTML = actual;
+}
+
+function clickOcho(){
+    actual +=8;
+        numActual.innerHTML = actual;
+}
+
+function clickNueve(){
+    actual +=9;
+        numActual.innerHTML = actual;
+}
+
+function clickCero(){
+    if(actual!=''){
+        actual += 0;
+        numActual.innerHTML = actual;
+    }
+}
+
 function clickSuma(){
     if(actual != ''){
         resultado += parseInt(actual);
@@ -33,4 +100,62 @@ function clickSuma(){
         numActual.innerHTML='0';
         resultadoElemento.innerHTML = resultado;
     }
+}
+
+function clickResta(){
+    if((actual != '')&&(resultadoElemento.innerHTML!=0)){
+        resultado -= parseInt(actual);
+        actual='';
+        numActual.innerHTML='0';
+        resultadoElemento.innerHTML = resultado;
+    }else if(resultadoElemento.innerHTML==0){
+        //debido a que es el primero n'umero y eventualmente no tiene con qui'en restarse.
+        firstTime();
+    }
+}
+
+function clickProducto(){
+    if((actual != '')&&(resultadoElemento.innerHTML!=0)){
+        resultado *= parseInt(actual);
+        actual='';
+        numActual.innerHTML='0';
+        resultadoElemento.innerHTML = resultado;
+    }else if(resultadoElemento.innerHTML==0){
+        firstTime();
+    }
+}
+
+function clickDivision(){
+    if((actual != '')&&(resultadoElemento.innerHTML!=0)){
+        resultado /= parseInt(actual);
+        actual='';
+        numActual.innerHTML='0';
+        resultadoElemento.innerHTML = resultado;
+    }else if(resultadoElemento.innerHTML==0){
+        firstTime();
+    }
+}
+
+function firstTime(){
+        resultado = parseInt(actual);
+        actual='';
+        numActual.innerHTML='0';
+        resultadoElemento.innerHTML=resultado;
+}
+
+function clickBack(){
+    if((actual != '')&&(numActual.innerHTML.length!=1)){
+        actual = actual.slice(0,actual.length-1); 
+        numActual.innerHTML = actual;
+    }else{
+        actual='';
+        numActual.innerHTML = '0';
+    }
+}
+
+function clickClean(){
+        actual='';
+        resultado=0;
+        numActual.innerHTML='0';
+        resultadoElemento.innerHTML=resultado;
 }
