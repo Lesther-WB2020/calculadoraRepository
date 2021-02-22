d4b5c87efe085fecc239274704cba377e7c1c58f
LESTHER-WB2020 XM <lesther.manuel@galileo.edu>
2021-02-22 10:41:03 -0600

    first comit

---
+++ index.js
@@ -0,0 +1,36 @@
+const mousetrap = require('mousetrap');
+//comentario
+mousetrap.bind('1',clickUno);
+mousetrap.bind('2',clickDos);
+mousetrap.bind('+',clickSuma);
+
+
+document.getElementById('botonUno').addEventListener('click',clickUno);
+document.getElementById('botonDos').addEventListener('click',clickDos);
+document.getElementById('suma').addEventListener('click',clickSuma);
+//document.getElementById('botonUno').onclick=clickUno;
+
+var numActual = document.getElementById('numeroActual');
+var resultadoElemento = document.getElementById('resultado');
+
+var actual = '';
+var resultado = 0;
+
+function clickUno(){
+    actual +=1;
+        numActual.innerHTML = actual;
+}
+
+function clickDos(){
+    actual +=2;
+        numActual.innerHTML = actual;
+}
+
+function clickSuma(){
+    if(actual != ''){
+        resultado += parseInt(actual);
+        actual='';
+        numActual.innerHTML='0';
+        resultadoElemento.innerHTML = resultado;
+    }
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+// ESTE ES EL PUNTO DE ENTRADA DE LA APP DE ELECTRÓN.
+const{app, BrowserWindow} = require('electron')
+//app es como la app en si
+//browserwindow es una ventana de navegador
+//eventualmente todo esto requiere de electron.
+//comentario para porobar git
+function createWindow(){
+    const ventana = new BrowserWindow({
+        width:350,
+        height:250,
+        webPreferences:{
+            nodeIntegration:true
+        }
+    })
+    ventana.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
